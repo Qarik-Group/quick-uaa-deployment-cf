ab681dfd335be6bd0474e1933fcfdea4c5c0315d
Dr Nic Williams <drnicwilliams@gmail.com>
2019-04-01 10:19:48 -0400

    delete ci/release_notes.md first, then create git commit

--- ci/scripts/shipit.sh
+++ ci/scripts/shipit.sh
@@ -49,6 +49,24 @@ fi
 git clone "${REPO_ROOT}" "${REPO_OUT}"
 git clone "${REPO_ROOT_MASTER}" "${REPO_OUT_MASTER}"
 
+header "Generate Github release & Slack notification"
+mkdir -p ${RELEASE_ROOT}/artifacts
+echo "v${VERSION}"                 > ${RELEASE_ROOT}/tag
+echo "v${VERSION}"                 > ${RELEASE_ROOT}/name
+mv ${REPO_OUT}/ci/release_notes.md   ${RELEASE_ROOT}/notes.md
+cat >> ${RELEASE_ROOT}/notes.md <<EOF
+
+### Versions
+\`\`\`plain
+$(cat ${REPO_OUT_MASTER}/.versions)
+\`\`\`
+EOF
+
+cat > ${NOTIFICATION_OUT:-notifications}/message <<EOS
+New ${GITHUB_REPO} v${VERSION} released. <https://github.com/${GITHUB_OWNER}/${GITHUB_REPO}/releases/tag/v${VERSION}|Release notes>.
+EOS
+
+
 header "Update git repo for release of ${VERSION}"
 pushd "${REPO_OUT}" > /dev/null
 
@@ -74,20 +92,4 @@ header "Merge to master"
 pushd "${REPO_OUT_MASTER}" > /dev/null
 git pull ../${REPO_OUT} -X theirs --no-edit
 popd > /dev/null
-
-mkdir -p ${RELEASE_ROOT}/artifacts
-echo "v${VERSION}"                 > ${RELEASE_ROOT}/tag
-echo "v${VERSION}"                 > ${RELEASE_ROOT}/name
-mv ${REPO_OUT}/ci/release_notes.md   ${RELEASE_ROOT}/notes.md
-cat >> ${RELEASE_ROOT}/notes.md <<EOF
-
-### Versions
-\`\`\`plain
-$(cat ${REPO_OUT_MASTER}/.versions)
-\`\`\`
-EOF
-
-cat > ${NOTIFICATION_OUT:-notifications}/message <<EOS
-New ${GITHUB_REPO} v${VERSION} released. <https://github.com/${GITHUB_OWNER}/${GITHUB_REPO}/releases/tag/v${VERSION}|Release notes>.
-EOS
 
